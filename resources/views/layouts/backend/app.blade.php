32113ba152189055e2a45d09f5f7b26fc4cb45e2
Eyasin Arafat <dolkd@gmail.com>
2019-01-12 00:11:04 +0600

    Layout complete with auth admin and author

---
+++ resources/views/layouts/backend/app.blade.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
+
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap Core Css -->
+    <link href="{{ asset('assets/backend/plugins/bootstrap/css/bootstrap.css') }}" rel="stylesheet">
+
+    <!-- Waves Effect Css -->
+    <link href="{{ asset('assets/backend/plugins/node-waves/waves.css') }}" rel="stylesheet" />
+
+    <!-- Animation Css -->
+    <link href="{{ asset('assets/backend/plugins/animate-css/animate.css') }}" rel="stylesheet" />
+
+    <!-- Morris Chart Css-->
+    <link href="{{ asset('assets/backend/plugins/morrisjs/morris.css') }}" rel="stylesheet" />
+
+    <!-- Custom Css -->
+    <link href="{{ asset('assets/backend/css/style.css') }}" rel="stylesheet">
+
+    <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
+    <link href="{{ asset('assets/backend/css/themes/all-themes.css') }}" rel="stylesheet" />
+
+    @stack('css')
+</head>
+<body class="theme-blue">
+
+<div class="page-loader-wrapper">
+    <div class="loader">
+        <div class="preloader">
+            <div class="spinner-layer pl-red">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div>
+                <div class="circle-clipper right">
+                    <div class="circle"></div>
+                </div>
+            </div>
+        </div>
+        <p>Please wait...</p>
+    </div>
+</div>
+<!-- #END# Page Loader -->
+<!-- Overlay For Sidebars -->
+<div class="overlay"></div>
+<!-- #END# Overlay For Sidebars -->
+<!-- Search Bar -->
+<div class="search-bar">
+    <div class="search-icon">
+        <i class="material-icons">search</i>
+    </div>
+    <input type="text" placeholder="START TYPING...">
+    <div class="close-search">
+        <i class="material-icons">close</i>
+    </div>
+</div>
+<!-- #END# Search Bar -->
+<!-- Top Bar -->
+    @include('layouts.backend.partial.navbar')
+<!-- #Top Bar -->
+<section>
+    <!-- Left Sidebar -->
+   @include('layouts.backend.partial.sidebar')
+    <!-- #END# Left Sidebar -->
+    <!-- Right Sidebar -->
+    <aside id="rightsidebar" class="right-sidebar">
+        <ul class="nav nav-tabs tab-nav-right" role="tablist">
+            <li role="presentation" class="active"><a href="#skins" data-toggle="tab">SKINS</a></li>
+            <li role="presentation"><a href="#settings" data-toggle="tab">SETTINGS</a></li>
+        </ul>
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane fade in active in active" id="skins">
+                <ul class="demo-choose-skin">
+                    <li data-theme="red" class="active">
+                        <div class="red"></div>
+                        <span>Red</span>
+                    </li>
+                    <li data-theme="pink">
+                        <div class="pink"></div>
+                        <span>Pink</span>
+                    </li>
+                    <li data-theme="purple">
+                        <div class="purple"></div>
+                        <span>Purple</span>
+                    </li>
+                    <li data-theme="deep-purple">
+                        <div class="deep-purple"></div>
+                        <span>Deep Purple</span>
+                    </li>
+                    <li data-theme="indigo">
+                        <div class="indigo"></div>
+                        <span>Indigo</span>
+                    </li>
+                    <li data-theme="blue">
+                        <div class="blue"></div>
+                        <span>Blue</span>
+                    </li>
+                    <li data-theme="light-blue">
+                        <div class="light-blue"></div>
+                        <span>Light Blue</span>
+                    </li>
+                    <li data-theme="cyan">
+                        <div class="cyan"></div>
+                        <span>Cyan</span>
+                    </li>
+                    <li data-theme="teal">
+                        <div class="teal"></div>
+                        <span>Teal</span>
+                    </li>
+                    <li data-theme="green">
+                        <div class="green"></div>
+                        <span>Green</span>
+                    </li>
+                    <li data-theme="light-green">
+                        <div class="light-green"></div>
+                        <span>Light Green</span>
+                    </li>
+                    <li data-theme="lime">
+                        <div class="lime"></div>
+                        <span>Lime</span>
+                    </li>
+                    <li data-theme="yellow">
+                        <div class="yellow"></div>
+                        <span>Yellow</span>
+                    </li>
+                    <li data-theme="amber">
+                        <div class="amber"></div>
+                        <span>Amber</span>
+                    </li>
+                    <li data-theme="orange">
+                        <div class="orange"></div>
+                        <span>Orange</span>
+                    </li>
+                    <li data-theme="deep-orange">
+                        <div class="deep-orange"></div>
+                        <span>Deep Orange</span>
+                    </li>
+                    <li data-theme="brown">
+                        <div class="brown"></div>
+                        <span>Brown</span>
+                    </li>
+                    <li data-theme="grey">
+                        <div class="grey"></div>
+                        <span>Grey</span>
+                    </li>
+                    <li data-theme="blue-grey">
+                        <div class="blue-grey"></div>
+                        <span>Blue Grey</span>
+                    </li>
+                    <li data-theme="black">
+                        <div class="black"></div>
+                        <span>Black</span>
+                    </li>
+                </ul>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="settings">
+                <div class="demo-settings">
+                    <p>GENERAL SETTINGS</p>
+                    <ul class="setting-list">
+                        <li>
+                            <span>Report Panel Usage</span>
+                            <div class="switch">
+                                <label><input type="checkbox" checked><span class="lever"></span></label>
+                            </div>
+                        </li>
+                        <li>
+                            <span>Email Redirect</span>
+                            <div class="switch">
+                                <label><input type="checkbox"><span class="lever"></span></label>
+                            </div>
+                        </li>
+                    </ul>
+                    <p>SYSTEM SETTINGS</p>
+                    <ul class="setting-list">
+                        <li>
+                            <span>Notifications</span>
+                            <div class="switch">
+                                <label><input type="checkbox" checked><span class="lever"></span></label>
+                            </div>
+                        </li>
+                        <li>
+                            <span>Auto Updates</span>
+                            <div class="switch">
+                                <label><input type="checkbox" checked><span class="lever"></span></label>
+                            </div>
+                        </li>
+                    </ul>
+                    <p>ACCOUNT SETTINGS</p>
+                    <ul class="setting-list">
+                        <li>
+                            <span>Offline</span>
+                            <div class="switch">
+                                <label><input type="checkbox"><span class="lever"></span></label>
+                            </div>
+                        </li>
+                        <li>
+                            <span>Location Permission</span>
+                            <div class="switch">
+                                <label><input type="checkbox" checked><span class="lever"></span></label>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </aside>
+    <!-- #END# Right Sidebar -->
+</section>
+
+<section class="content">
+    @yield('content')
+</section>
+
+<!-- Jquery Core Js -->
+<script src="{{ asset('assets/backend/plugins/jquery/jquery.min.js') }}"></script>
+
+<!-- Bootstrap Core Js -->
+<script src="{{ asset('assets/backend/plugins/bootstrap/js/bootstrap.js') }}"></script>
+
+<!-- Select Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/bootstrap-select/js/bootstrap-select.js') }}"></script>
+
+<!-- Slimscroll Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/jquery-slimscroll/jquery.slimscroll.js') }}"></script>
+
+<!-- Waves Effect Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/node-waves/waves.js') }}"></script>
+
+<!-- Jquery CountTo Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/jquery-countto/jquery.countTo.js') }}"></script>
+
+<!-- Morris Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/raphael/raphael.min.js') }}"></script>
+<script src="{{ asset('assets/backend/plugins/morrisjs/morris.js') }}"></script>
+
+<!-- ChartJs -->
+<script src="{{ asset('assets/backend/plugins/chartjs/Chart.bundle.js') }}"></script>
+
+<!-- Flot Charts Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.js') }}"></script>
+<script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.resize.js') }}"></script>
+<script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.pie.js') }}"></script>
+<script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.categories.js') }}"></script>
+<script src="{{ asset('assets/backend/plugins/flot-charts/jquery.flot.time.js') }}"></script>
+
+<!-- Sparkline Chart Plugin Js -->
+<script src="{{ asset('assets/backend/plugins/jquery-sparkline/jquery.sparkline.js') }}"></script>
+
+<!-- Custom Js -->
+<script src="{{ asset('assets/backend/js/admin.js') }}"></script>
+<script src="{{ asset('assets/backend/js/pages/index.js') }}"></script>
+
+<!-- Demo Js -->
+<script src="{{ asset('assets/backend/js/demo.js') }}"></script>
+
+@stack('js')
+</body>
+</html>
---
+++ resources/views/layouts/frontend/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+
+    <link href="{{ asset('assets/frontend/css/bootstrap.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('assets/frontend/css/swiper.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('assets/frontend/css/ionicons.css') }}" rel="stylesheet">
+
+    @stack('css')
+</head>
+<body>
+    @include('layouts.frontend.partial.header')
+
+    @yield('content')
+
+    @include('layouts.frontend.partial.footer')
+
+    <script src="{{ asset('assets/frontend/js/jquery-3.1.1.min.js') }}"></script>
+
+    <script src="{{ asset('assets/frontend/js/tether.min.js') }}"></script>
+
+    <script src="{{ asset('assets/frontend/js/bootstrap.js') }}"></script>
+
+
+    <script src="{{ asset('assets/frontend/js/scripts.js') }}"></script>
+
+    @stack('js')
+</body>
+</html>
